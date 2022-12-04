cc4c6b9d80ac392fe235c97829e1ac60331360cf
fenixzava <fenixzava@gmail.com>
2022-12-04 21:58:37 +0300

    create metod SortingArray

--- task/Program.cs
+++ task/Program.cs
@@ -5,3 +5,15 @@
 string[] array1 = new string[5] {"123", "911", "hello", "world", "GB"};
 string[] array2 = new string[array1.Length];
 int count = 0;
+
+void SortingArray(string[] array1, string[] array2)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
